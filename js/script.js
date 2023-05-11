771fa109cc674b6f949fc42cfc50b9dbf43d9284
emanuelefarelli <emaaliens888@gmail.com>
2023-05-11 15:53:42 +0200

    Svolto l'esercizio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const yourName = prompt("Insert your name:");
+const yourSurname = prompt("Insert your surname:");
+const favouriteColor = prompt("Insert your favourite color:");
+
+document.getElementById('your-password').innerHTML = yourName + yourSurname + favouriteColor + '21';
+
